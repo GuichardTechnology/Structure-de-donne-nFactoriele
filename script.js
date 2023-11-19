ef27c33556e192e0497158c397d7907f2c2e1514
GuichardTechnology <guichardj483@gmail.com>
2023-11-19 18:27:48 -0300

    homework Factorielle

---
+++ script.js
@@ -0,0 +1,22 @@
+     /*function nfactorielle(n){
+        debugger
+        
+        if(n === 1 || n === 0) return 1
+        if(n < 0 ) return "error"
+
+        return n * nfactorielle( n - 1 )
+
+     }
+
+     console.log(nfactorielle(7))*/
+
+     var factorielle = 1;
+      debugger
+
+     for(let num = 1; num <= 7; num++){
+
+      factorielle =  factorielle * num
+
+     }
+
+     console.log(factorielle)
